3ab8771fa38a4a6d33827f61c4157341e6a24f2e
futurist <jamesyang999@gmail.com>
2018-04-26 17:55:19 +0800

    clean

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-var performance = typeof window !== 'undefined' && window.performance
-var hrtime = typeof process !== 'undefined' && process
-  && typeof process.hrtime === 'function' && process.hrtime
-
-function now() {
-  if(performance!==false) return performance.now()
-  else if(hrtime!==false) {
-    var t = hrtime()
-    return (t[0] + t[1] / 1e9) * 1000
-  } else {
-    return Date.now()
-  }
-}
 
 class TimeSequence {
   constructor (seqArr, config) {
